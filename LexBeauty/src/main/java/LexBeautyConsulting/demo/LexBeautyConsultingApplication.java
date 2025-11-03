8697be1472c380d3a60f888c43c06c4bf3fe0a07
RachelRamirezMadriz <rramirez80534@ufide.ac.cr>
2025-11-03 15:51:05 -0600

    Estructura y Inicialización del proyecto final MVC
    En este commit se inicializó el proyecto en java con Springboot, se inicio con las herramientas necesarias para el desarrollo del proyecto

---
+++ LexBeauty/src/main/java/LexBeautyConsulting/demo/LexBeautyConsultingApplication.java
@@ -0,0 +1,13 @@
+package LexBeautyConsulting.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexBeautyConsultingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LexBeautyConsultingApplication.class, args);
+	}
+
+}
---
+++ LexBeauty/src/test/java/LexBeautyConsulting/demo/LexBeautyConsultingApplicationTests.java
@@ -0,0 +1,13 @@
+package LexBeautyConsulting.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LexBeautyConsultingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
