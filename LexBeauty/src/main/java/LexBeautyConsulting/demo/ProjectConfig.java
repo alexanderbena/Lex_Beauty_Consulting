bd3edc78e56b58fe765fe29e8fc8d49d4473bcb3
RachelRamirezMadriz <rramirez80534@ufide.ac.cr>
2025-11-03 17:41:46 -0600

    Configuración inicial y dependencias del proyecto MVC
    Se realizó la configuración del proyecto al igual que las properties donde se realizo la asignación del puerto 90, la conexión con firebase, MySQL y los logs

---
+++ LexBeauty/src/main/java/LexBeautyConsulting/demo/ProjectConfig.java
@@ -0,0 +1,60 @@
+package LexBeautyConsulting.demo;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Locale;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+public class ProjectConfig implements WebMvcConfigurer{
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        /*Listo para generar otras vistas*/
+    }
+    
+    /*Configuracion de Thymeleaf*/
+    @Bean
+    public SpringResourceTemplateResolver templateResolver_0() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setOrder(0);
+        resolver.setCheckExistence(true);
+        return resolver;
+    }
+    /*Config del JSON con las credenciales de la BD en Firebase*/
+    @Value("${firebase.json.path}")
+    private String jsonPath;
+
+    @Value("${firebase.json.file}")
+    private String jsonFile;
+
+    @Bean
+    public Storage storage() throws IOException {
+        ClassPathResource resource = new ClassPathResource(jsonPath + File.separator + jsonFile);
+        try (InputStream inputStream = resource.getInputStream()) {
+            GoogleCredentials credentials = GoogleCredentials.fromStream(inputStream);
+            return StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+        }
+    }
+}
